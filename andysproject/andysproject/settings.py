0ac4a735487076619b804cd34f6e6c55c87d733e
Andy Wade <andyandamandawade@hotmail.com>
2021-07-08 13:00:02 -0500

    completed section: adding local settings

--- andysproject/andysproject/settings.py
+++ andysproject/andysproject/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'guylp$trehow*2vh+cth$c932dj-&^j=vp!y=v(x+nj-#1j1h6'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,17 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'andysproject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -118,3 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from andysproject.local_settings import *
+except ImportError:
+    pass
